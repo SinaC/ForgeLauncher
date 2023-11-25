2fca56c7c5d4ac79a043e9c6d2a59303735d95d3
SinaC <heymbeeck.joel@gmail.com>
2023-11-25 11:15:51 +0100

    Download, unzip and launch added

---
+++ ForgeLauncher.WPF/Extract.cs
@@ -0,0 +1,39 @@
+﻿using SharpCompress.Common;
+using SharpCompress.Readers;
+using System.IO;
+using System.Linq;
+
+namespace ForgeLauncher.WPF
+{
+    public class Extract
+    {
+        public void ExtractTarBz2(string archiveName, string destinationFolder)
+        {
+            using var stream = File.OpenRead(archiveName);
+            using var reader = ReaderFactory.Open(stream);
+            while (reader.MoveToNextEntry())
+            {
+                if (!reader.Entry.IsDirectory)
+                {
+                    if (reader.Entry.Key.Any(x => x < 32))
+                    {
+                        var fixedEntryKeyAsCharArray = reader.Entry.Key.Select(x => x < 32 ? '_' : x).ToArray();
+                        var fixedEntryKey = new string(fixedEntryKeyAsCharArray);
+                        var filename = Path.Combine(destinationFolder, fixedEntryKey);
+                        reader.WriteEntryToFile(filename, new ExtractionOptions
+                        {
+                            ExtractFullPath = true,
+                            Overwrite = true
+                        });
+                    }
+                    else
+                        reader.WriteEntryToDirectory(destinationFolder, new ExtractionOptions
+                        {
+                            ExtractFullPath = true,
+                            Overwrite = true
+                        });
+                }
+            }
+        }
+    }
+}
