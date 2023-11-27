daeaa5b96e3148e0e8d011678c198e56032f50fe
Joël Heymbeeck <joel.heymbeeck@externel.be>
2023-11-27 09:35:39 +0100

    Settings manager added

--- ForgeLauncher.WPF/SettingsManager.cs
+++ ForgeLauncher.WPF/SettingsManager.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace ForgeLauncher.WPF
 {
@@ -10,32 +10,65 @@ namespace ForgeLauncher.WPF
         private const string SettingFileName = "ForgeLauncher.settings";
         private const string DefaultDailySnapshotUrl = "https://downloads.cardforge.org/dailysnapshots/";
 
+        private const string ForgeInstallationFolderKey = "ForgeInstallationFolder";
+        private const string DailySnapshotsUrlKey = "DailySnapshotsUrl";
+
         private Dictionary<string, string> Settings { get; } = new Dictionary<string, string>();
 
         public SettingsManager()
         {
+            Load();
         }
 
-        public string ForgeInstallationFolder => GetOrDefault(Settings, "ForgeInstallationFolder", System.AppDomain.CurrentDomain.BaseDirectory);
-        public string DailySnapshotsUrl => GetOrDefault(Settings, "DailySnapshotsUrl", DefaultDailySnapshotUrl);
+        public string ForgeInstallationFolder
+        {
+            get => GetOrDefault(Settings, ForgeInstallationFolderKey, AppDomain.CurrentDomain.BaseDirectory);
+            set
+            {
+                Settings[ForgeInstallationFolderKey] = value;
+                Save();
+            }
+        }
+        public string DailySnapshotsUrl
+        {
+            get => GetOrDefault(Settings, DailySnapshotsUrlKey, DefaultDailySnapshotUrl);
+            set
+            {
+                Settings[DailySnapshotsUrlKey] = value;
+                Save();
+            }
+        }
 
-        public async Task Load()
+        private void Load()
         {
             if (File.Exists(SettingFileName))
             {
-                var json = File.ReadAllText(SettingFileName);
-                var dictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
-                Settings.Clear();
-                foreach (var kv in dictionary)
-                    Settings.Add(kv.Key, kv.Value);
+                try
+                {
+                    var json = File.ReadAllText(SettingFileName);
+                    var dictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+                    Settings.Clear();
+                    foreach (var kv in dictionary)
+                        Settings.Add(kv.Key, kv.Value);
+                }
+                catch (Exception ex)
+                {
+                    //TODO
+                }
             }
-            // TODO: set default value
         }
 
-        public void Save()
+        private void Save()
         {
-            var json = JsonSerializer.Serialize(Settings);
-            File.WriteAllText(json, SettingFileName);
+            try
+            {
+                var json = JsonSerializer.Serialize(Settings);
+                File.WriteAllText(json, SettingFileName);
+            }
+            catch (Exception ex)
+            {
+                //TODO
+            }
         }
 
         private static TValue GetOrDefault<TKey, TValue>(IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
